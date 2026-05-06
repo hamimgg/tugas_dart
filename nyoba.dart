e436c57f592993462c0987f38afb8b6fec6c59bd
hamimgg <hamim.abdillah1656@gmail.com>
2026-05-06 13:49:45 +0700

    tugasdua

--- nyoba.dart
+++ nyoba.dart
@@ -1,22 +1,3 @@
-/*void main (){
-  int uts = 60;
-  int uas = 79;
-  double kehadiran = 0.75;
-  double rataRata = (uts + uas) / 2;
-  bool lulus = (rataRata >= 60 && kehadiran >= 0.75);
-
-  print("nilai uts anda: $uts");
-  print("nilai uas anda: $uas");
-  print("rata-rata anda: $rataRata");
-  
-
-  if (lulus) {
-    print("anda lulus");
-  } else {
-    print("Mohon maaf anda tidak lulus");
-  }
-}*/
-
 import 'dart:io';
 
 void main() {
@@ -53,5 +34,7 @@ print("kehadiran:$inputkehadiran");
 if (lulus){
   print ("Selamat anda lulus!");
 } else {print ("Anda dinyatakan tidak lulus!");}
+
+
 
 }
