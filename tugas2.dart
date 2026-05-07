aaccf9a99959f11f00abd3890bf2770f35e4ecbe
hamimgg <hamim.abdillah1656@gmail.com>
2026-05-07 09:46:27 +0700

    tugas3

--- tugas2.dart
+++ tugas2.dart
@@ -1,22 +1,3 @@
-/*void main (){
-  int uts = 60;
-  int uas = 79;
-  double kehadiran = 0.75;
-  double rataRata = (uts + uas) / 2;
-  bool lulus = (rataRata >= 60 && kehadiran >= 0.75);
-
-  print("nilai uts anda: $uts");
-  print("nilai uas anda: $uas");
-  print("rata-rata anda: $rataRata");
-  
-
-  if (lulus) {
-    print("anda lulus");
-  } else {
-    print("Mohon maaf anda tidak lulus");
-  }
-}*/
-
 import 'dart:io';
 
 void main() {
